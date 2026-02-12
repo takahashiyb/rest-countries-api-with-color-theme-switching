bd99502b6bf1debf877cf04581e1699a31d6ddad
takahashiyb <takahashiyb89@gmail.com>
2026-02-13 01:39:59 +0800

    Add HomePage route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,13 @@
 import DesignSystem from '@/design-system/DesignSystem.vue'
+import HomePage from '@/pages/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/design-system', component: DesignSystem }],
+  routes: [
+    { path: '/design-system', component: DesignSystem },
+    { path: '/', component: HomePage },
+  ],
 })
 
 export default router
