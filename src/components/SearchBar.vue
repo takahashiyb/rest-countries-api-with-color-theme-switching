35bec8ec97b23d313e8bd73a398ba5dafc0ea53e
takahashiyb <takahashiyb89@gmail.com>
2026-02-13 01:42:04 +0800

    Create a SearchBar.vue

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { useColorMode } from '@/stores/color-mode'
+
+const color = useColorMode()
+</script>
+<template>
+  <div :class="color.mode">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+      <!-- !Font Awesome Free 7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc. -->
+      <path
+        fill="#C4C4C4"
+        d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"
+      />
+    </svg>
+    <input type="text" placeholder="Search for a country..." />
+  </div>
+</template>
+<style scoped>
+div {
+  --background-color: var(--light-bg);
+  --placeholder-color: hsl(var(--grey-250));
+
+  display: flex;
+  gap: var(--spacing-0300);
+
+  height: var(--spacing-0200);
+  background-color: var(--background-color);
+
+  padding-top: var(--spacing-0200);
+  padding-inline: var(--spacing-0400);
+  padding-bottom: var(--spacing-0200);
+
+  box-shadow: 0.1em 0.1em 1em hsl(var(--black), 0.15);
+}
+
+div.dark {
+  --background-color: var(--dark-bg);
+  --placeholder-color: var(--dark-clr);
+}
+
+input {
+  flex: 1;
+
+  background-color: var(--background-color);
+  font: var(--font-preset-6r);
+
+  border: none;
+}
+
+input::placeholder {
+  color: var(--placeholder-color);
+}
+
+path {
+  fill: var(--placeholder-color);
+}
+
+@media (min-width: 47.5em) {
+  div {
+    padding-top: var(--spacing-0400);
+    padding-inline: var(--spacing-0200);
+    padding-bottom: var(--spacing-0400);
+  }
+
+  input {
+    color: hsl(var(--grey-400));
+    font: var(--font-preset-5r);
+  }
+
+  path {
+    fill: hsl(var(--grey-400));
+  }
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SearchBar from '@/components/SearchBar.vue'
+</script>
+<template>
+  <main>
+    <SearchBar />
+  </main>
+</template>
+<style scoped></style>
