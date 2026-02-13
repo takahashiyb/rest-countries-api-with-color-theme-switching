484a317c19c644d7e2b83c7690546e60f966cd65
takahashiyb <takahashiyb89@gmail.com>
2026-02-13 16:44:02 +0800

    Set up router link for country page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import DesignSystem from '@/design-system/DesignSystem.vue'
+import CountryPage from '@/pages/CountryPage.vue'
 import HomePage from '@/pages/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
@@ -7,6 +8,7 @@ const router = createRouter({
   routes: [
     { path: '/design-system', component: DesignSystem },
     { path: '/', component: HomePage },
+    { path: '/country/', component: CountryPage },
   ],
 })
 
