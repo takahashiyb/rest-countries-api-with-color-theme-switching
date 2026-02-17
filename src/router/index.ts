d8cbc95ece452e7cb1290a65f027f8a7d2513bbd
takahashiyb <takahashiyb89@gmail.com>
2026-02-17 10:57:45 +0800

    Add router guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,34 @@ import DesignSystem from '@/design-system/DesignSystem.vue'
 import CountryPage from '@/pages/CountryPage.vue'
 import HomePage from '@/pages/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { useStoreData } from '@/stores/data'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/design-system', component: DesignSystem },
     { path: '/', component: HomePage },
-    { path: '/country/', component: CountryPage },
+    {
+      path: '/country/',
+      component: CountryPage,
+      beforeEnter: (to, from, next) => {
+        if (!to.query.name) {
+          next({ path: '/' })
+        } else {
+          next()
+        }
+      },
+    },
   ],
+})
+
+router.beforeResolve(async (to, from) => {
+  const store = useStoreData()
+  if (!store.typedData) {
+    await store.loadData()
+  }
+  console.log('Navigating to:', to.fullPath)
+  return true
 })
 
 export default router
